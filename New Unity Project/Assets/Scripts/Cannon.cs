e382a4fa0c0730ebb61bd81280c4dfbdd611cd3a
ArKaDiA_Dev <jfernandorojasc@gmail.com>
2020-03-28 10:20:55 -0500

    Cannon Commit

---
+++ New Unity Project/Assets/Scripts/Cannon.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField]
+    int Force;
+    int Ammo = 50;
+
+    Transform CameraCannon;
+
+    public Camera mainCamera;
+    public GameObject Ball;
+    public GameObject SpawnPoint;
+
+    GameObject BallInstance;
+
+   
+
+    void Start()
+    {
+        CameraCannon = gameObject.transform.GetChild(0);        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePosition = Input.mousePosition;
+        
+        Vector3 globalPosition = mainCamera.ScreenToWorldPoint(new Vector3(
+            mousePosition.x,
+            mousePosition.y,
+            CameraCannon.position.z - mainCamera.transform.position.z
+            ));        
+
+        CameraCannon.transform.LookAt(globalPosition);
+
+
+        if (Input.GetMouseButtonDown(0) && Ammo > 0)
+        {
+            BallInstance = Instantiate(Ball, SpawnPoint.transform.position, Ball.transform.rotation);
+            Rigidbody BallridgyBody = BallInstance.GetComponent<Rigidbody>();
+            
+            BallridgyBody.AddForce((globalPosition - CameraCannon.transform.position)*Force);
+            Ammo--;
+        }
+        
+
+
+
+
+
+    }
+}
---
+++ New Unity Project/Assets/Scripts/RopeGenerator.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeGenerator : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int offsetSize;
+
+    public GameObject pointA;
+    public GameObject pointB;
+    public GameObject Ropelink;
+
+    public GameObject[] Ropelinks;
+
+    Rigidbody rgbPointA;
+    Rigidbody rgbPointB;
+
+
+    CapsuleCollider ColliderRopelink;
+
+
+
+
+    void Start()
+    {
+        rgbPointA = pointA.GetComponent<Rigidbody>();
+        rgbPointB = pointB.GetComponent<Rigidbody>();
+        ColliderRopelink = Ropelink.GetComponent<CapsuleCollider>();
+
+        float dis = Vector3.Distance(pointB.transform.position, pointA.transform.position);
+        int NumberofJoints = Mathf.RoundToInt(dis / ColliderRopelink.height) + offsetSize;
+        Ropelinks = new GameObject[NumberofJoints];
+
+        for (int i = 0; i < NumberofJoints; i++)
+        {
+            GameObject link = Instantiate(Ropelink);
+            FixedJoint joint = link.GetComponent<FixedJoint>();
+
+            Ropelinks[i] = link;
+            if (i == 0)
+            {
+                joint.connectedBody = rgbPointA;                
+            }
+            else if(i != 0 && i < NumberofJoints-1)
+            {
+                joint.connectedBody = Ropelinks[i - 1].GetComponent<Rigidbody>();
+            }
+            else
+            {
+                joint.connectedBody = Ropelinks[i - 1].GetComponent<Rigidbody>();
+                FixedJoint secondJointlink = link.AddComponent<FixedJoint>();
+                secondJointlink.connectedBody = rgbPointB;
+            }        
+                
+            
+
+            
+            
+        }
+
+        
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
